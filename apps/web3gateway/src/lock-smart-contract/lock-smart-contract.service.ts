28edd95a68274a5d7f985fa2b78bca5d5c5d3078
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-06-26 14:26:41 -0300

    feat(LockSmartContractService): lock contract object constructor

--- apps/web3gateway/src/lock-smart-contract/lock-smart-contract.service.ts
+++ apps/web3gateway/src/lock-smart-contract/lock-smart-contract.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Lock__factory, Lock } from '@/smart-contracts';
+import ethers from 'ethers';
 
 @Injectable()
-export class LockSmartContractService {}
+export class LockSmartContractService {
+
+  private provider = new ethers.providers.JsonRpcProvider(process.env.EVM_RPC_URL);
+  private signer: ethers.Wallet;
+  private lockContractFactory: Lock__factory;
+  private lockContract: Lock;
+
+  constructor() {
+    this.provider = new ethers.providers.JsonRpcProvider(process.env.EVM_RPC_URL);
+    this.signer = new ethers.Wallet(process.env.EVM_PRIVATE_KEY, this.provider);
+    this.lockContractFactory = new Lock__factory(this.signer);
+    this.lockContract = this.lockContractFactory.attach(process.env.LOCK_CONTRACT_ADDRESS);
+  }
+}
