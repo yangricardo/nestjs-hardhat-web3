c37b57da6ca2344529668df22245a32553fe8c91
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-06-26 13:47:38 -0300

    config: hardhat configuration to generate ethers 5 typechain

--- packages/smart-contracts/hardhat.config.ts
+++ packages/smart-contracts/hardhat.config.ts
@@ -1,6 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import "@nomiclabs/hardhat-solhint";
+import "@openzeppelin/hardhat-upgrades";
+import "@openzeppelin/test-helpers";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-ethers";
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
 };
